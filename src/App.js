7e4eee2da06839bf0dbb1a8822b703489f8221bd
zakaria552 <zakaria.farah55@outlook.com>
2023-02-02 16:25:23 +0000

    component setup and basic styles applied

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import Game from './components/Game';
 import {PORT} from "./utils/constants"
 
 function App() {
+  let playerRole = "x" // two roles x or o
   const url = `ws://localhost:${PORT}`
   let wsClient;
   const init = () => {
@@ -16,11 +18,11 @@ function App() {
     }
   }
   
-  init()
+  // init()
   return (
-    <div className="App">
+    <div className="App flex flex-col items-center justify-center bg-emerald-200 h-screen">
       <h2 className='text-3xl'>hello</h2>
-      <button className="border-2" >start connection</button>
+      <Game playerRole={playerRole}/>
     </div>
   );
 }
