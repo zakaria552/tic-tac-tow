0ddd1e0070a7c7705800bf5e22338e317d0bae39
zakaria552 <zakaria.farah55@outlook.com>
2023-02-12 19:28:27 +0000

    offline feature

--- src/gameStateReducer.js
+++ src/gameStateReducer.js
@@ -12,16 +12,30 @@ export const initialState = {
     gameDraw: false,
     gameOver: false,
     gameMode: "find a game",
-    gamePin: ""
+    gamePin: "",
+    player1: false,
+    player2: false,
+    playerTurn: "", 
+    playerWon: ""
 }
 
 export const gameStateReducer = (gameState, action) => {
     let newState = {...gameState}
     newState.grid = [...gameState.grid]
     switch(action.type) {
+        case STATE.ACTION.PLAY_OFFLINE:
+            newState.player1 = "x"
+            newState.player2 = "o"
+            newState.startGame = true
+            newState.playerTurn = action.turn
+            newState.myTurn = true
+            return newState
         case STATE.ACTION.TURN_PLAYED:
             newState.myTurn = false
             newState.grid = action.payload.grid
+            return newState
+        case STATE.ACTION.PLAYER_TURN_PLAYED:
+            newState.playerTurn = action.turn
             return newState
         case STATE.ACTION.MY_TURN:
             newState.myTurn = true
@@ -38,10 +52,23 @@ export const gameStateReducer = (gameState, action) => {
             newState.gameWon = false
             newState.gameOver = false
             newState.myTurn = gameState.playerRole === action.payload.playerTurn
-            newState.gameStart = true
+            newState.startGame = true
             newState.grid = [["", "", ""], ["", "", ""],["", "", ""]]
             newState.message = ""
             newState.startGame = true
+            return newState
+        case STATE.ACTION.RESTART_OFFLINE:
+            let zeroOrOne = Math.floor(Math.random() * 2)
+            let player = ["player1", "player2"][zeroOrOne]
+            newState.gameDraw = false
+            newState.gameLost = false
+            newState.gameWon = false
+            newState.gameOver = false
+            newState.grid = [["", "", ""], ["", "", ""],["", "", ""]]
+            newState.playerTurn = {player, role: player === "player1" ? "x": "o"}
+            newState.playerWon = ""
+            newState.startGame = true
+            newState.myTurn = true
             return newState
         case  STATE.ACTION.CONNECT:
             newState.wsClient = action.payload.wsClient
@@ -54,6 +81,7 @@ export const gameStateReducer = (gameState, action) => {
             newState.gameOver = true
             return newState
         case STATE.ACTION.GAME_DRAW: 
+            console.log(action)
             newState.gameDraw = true
             newState.grid = action.payload.grid
             newState.gameOver = true
@@ -63,10 +91,17 @@ export const gameStateReducer = (gameState, action) => {
             newState.grid = action.payload.grid
             newState.gameOver = true
             return newState
+        case STATE.ACTION.GAME_OVER:
+            newState.playerWon = action.playerWon
+            newState.gameOver = true
+            newState.myTurn = false
+            return newState
         case STATE.ACTION.CANCEL_SEARCH:
             newState = initialState
             newState.wsClient = gameState.wsClient
             newState.serverStats = gameState.serverStats
+            newState.grid = [["", "", ""], ["", "", ""],["", "", ""]]
+            console.log(newState)
             return newState
         case STATE.ACTION.CHANGE_GAME_MODE:
             console.log(action)
